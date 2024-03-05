964b40528118f5cbba758a7e5f8d2cac8bcb1db9
Zhiengzhengml <x00217230210@njit.edu.cn>
2024-03-05 19:59:35 +0800

    summit

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,5 +1,17 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class BubbleSort {
+    public void bubbleSort(int [] array){
+        for(int i=0; i<array.length-1;i++){
+            for(int j=0;j<array.length-i-1;j++){
+                if(array[j]>array[j+1]){
+                    System.out.println("i value is" + i + "and j value is" + j);
+                    int temporary = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temporary;
+            }
+            }
+        }
+        for(int j=0;j<array.length;j++){
+            System.out.println(array[j]+",");
+        }
     }
 }
